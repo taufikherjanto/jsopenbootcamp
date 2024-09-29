5485a7501d5990aa991643e7a756a8a5b76110e9
Taufik Herjanto <taufik.herjanto@live.com>
2024-09-29 21:44:04 +0700

    odify todo

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,13 +2,33 @@ const todo = [
     { judul: 'Buat website', status: 'design', tanggal: '2024-09-29'}
 ];
 
+function template(judul, status, index) {
+    const elementHTML = `
+        <li>
+            ${judul} | ${status} | @{NOW.format("YYYY-MM-DD")} | 
+            <button onclick="ubah(${index})">Ubah</button> 
+            <button onclick="hapus(${index})">Hapus</button>
+        </li>
+        `;
+    
+    return elementHTML;
+}
+
 function tambah() {
     const judul = prompt("Masukkan nama todo");
     const status = prompt("Masukkan status todo");
-    console.log(nama, status);
-    const dataBaru = {judul: nama, status: status};
-    console.log(dataBaru);
-    todo.push();
+    console.log(judul, status);
+    const databaru = {judul: judul, status: status};
+    console.log(databaru);
+
+    // menambahkan data ke variable todo
+    todo.push(databaru);
+
+    // update html
+    todo.map((value, index) => {
+        console.log(value, "Ini index ", index);
+        document.getElementById("data").innerHTML += template(value.judul, value.status, value.tanggal)
+    })
 }
 
 function ubah(index) {
--- tmp/js_todo.js
+++ tmp/js_todo.js
@@ -1,1 +1,7 @@
-const todo=[{name:'Buat website',status:'design',tanggal:'2024-09-29'}];function tambah(){const nama=prompt("Masukkan nama todo");const status=prompt("Masukkan status todo");console.log(nama,status);const dataBaru={name:nama,status:status};console.log(dataBaru);todo.push()}function ubah(index){alert(`fungsi ubah | ini adalah data dengan index ke ${index}`)}function hapus(index){alert(`fungsi hapus | ini adalah data dengan index ke ${index}`)}
+const todo=[{judul:'Buat website',status:'design',tanggal:'2024-09-29'}];function template(judul,status,index){const elementHTML=`
+        <li>
+            ${judul} | ${status} | 2024-09-29 | 
+            <button onclick="ubah(${index})">Ubah</button> 
+            <button onclick="hapus(${index})">Hapus</button>
+        </li>
+        `;return elementHTML}function tambah(){const judul=prompt("Masukkan nama todo");const status=prompt("Masukkan status todo");console.log(judul,status);const databaru={judul:judul,status:status};console.log(databaru);todo.push(databaru);todo.map((value,index)=>{console.log(value,"Ini index ",index);document.getElementById("data").innerHTML+=template(value.judul,value.status,value.tanggal)})}function ubah(index){alert(`fungsi ubah | ini adalah data dengan index ke ${index}`)}function hapus(index){alert(`fungsi hapus | ini adalah data dengan index ke ${index}`)}
